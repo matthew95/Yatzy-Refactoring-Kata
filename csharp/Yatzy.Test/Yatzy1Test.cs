61e513ef8c3f39121f46bdfd9bc5c836be259f1c
Guido Klijn <guidoklijn42@gmail.com>
2025-09-26 13:00:54 +0200

    start on kata

--- csharp/Yatzy.Test/Yatzy1Test.cs
+++ csharp/Yatzy.Test/Yatzy1Test.cs
@@ -1,4 +1,5 @@
 using Xunit;
+using static Yatzy1.Yatzy1;
 
 namespace Yatzy.Test
 {
@@ -8,32 +9,32 @@ namespace Yatzy.Test
         public void Chance_scores_sum_of_all_dice()
         {
             var expected = 15;
-            var actual = Yatzy1.Yatzy1.Chance(2, 3, 4, 5, 1);
+            var actual = Chance(2, 3, 4, 5, 1);
             Assert.Equal(expected, actual);
-            Assert.Equal(16, Yatzy1.Yatzy1.Chance(3, 3, 4, 5, 1));
+            Assert.Equal(16, Chance(3, 3, 4, 5, 1));
         }
 
         [Fact]
-        public void Fact_1s()
+        public void Fact_Ones()
         {
-            Assert.True(Yatzy1.Yatzy1.Ones(1, 2, 3, 4, 5) == 1);
-            Assert.Equal(2, Yatzy1.Yatzy1.Ones(1, 2, 1, 4, 5));
-            Assert.Equal(0, Yatzy1.Yatzy1.Ones(6, 2, 2, 4, 5));
-            Assert.Equal(4, Yatzy1.Yatzy1.Ones(1, 2, 1, 1, 1));
+            Assert.Equal(1, Ones([1, 2, 3, 4, 5]));
+            Assert.Equal(2, Ones([1, 2, 1, 4, 5]));
+            Assert.Equal(0, Ones([6, 2, 2, 4, 5]));
+            Assert.Equal(4, Ones([1, 2, 1, 1, 1]));
         }
 
         [Fact]
         public void Fact_2s()
         {
-            Assert.Equal(4, Yatzy1.Yatzy1.Twos(1, 2, 3, 2, 6));
-            Assert.Equal(10, Yatzy1.Yatzy1.Twos(2, 2, 2, 2, 2));
+            Assert.Equal(4, Twos(1, 2, 3, 2, 6));
+            Assert.Equal(10, Twos(2, 2, 2, 2, 2));
         }
 
         [Fact]
         public void Fact_threes()
         {
-            Assert.Equal(6, Yatzy1.Yatzy1.Threes(1, 2, 3, 2, 3));
-            Assert.Equal(12, Yatzy1.Yatzy1.Threes(2, 3, 3, 3, 3));
+            Assert.Equal(6, Threes(1, 2, 3, 2, 3));
+            Assert.Equal(12, Threes(2, 3, 3, 3, 3));
         }
 
         [Fact]
@@ -47,9 +48,9 @@ namespace Yatzy.Test
         [Fact]
         public void four_of_a_knd()
         {
-            Assert.Equal(12, Yatzy1.Yatzy1.FourOfAKind(3, 3, 3, 3, 5));
-            Assert.Equal(20, Yatzy1.Yatzy1.FourOfAKind(5, 5, 5, 4, 5));
-            Assert.Equal(12, Yatzy1.Yatzy1.FourOfAKind(3, 3, 3, 3, 3));
+            Assert.Equal(12, FourOfAKind(3, 3, 3, 3, 5));
+            Assert.Equal(20, FourOfAKind(5, 5, 5, 4, 5));
+            Assert.Equal(12, FourOfAKind(3, 3, 3, 3, 3));
         }
 
         [Fact]
@@ -63,16 +64,16 @@ namespace Yatzy.Test
         [Fact]
         public void fullHouse()
         {
-            Assert.Equal(18, Yatzy1.Yatzy1.FullHouse(6, 2, 2, 2, 6));
-            Assert.Equal(0, Yatzy1.Yatzy1.FullHouse(2, 3, 4, 5, 6));
+            Assert.Equal(18, FullHouse(6, 2, 2, 2, 6));
+            Assert.Equal(0, FullHouse(2, 3, 4, 5, 6));
         }
 
         [Fact]
         public void largeStraight()
         {
-            Assert.Equal(20, Yatzy1.Yatzy1.LargeStraight(6, 2, 3, 4, 5));
-            Assert.Equal(20, Yatzy1.Yatzy1.LargeStraight(2, 3, 4, 5, 6));
-            Assert.Equal(0, Yatzy1.Yatzy1.LargeStraight(1, 2, 2, 4, 5));
+            Assert.Equal(20, LargeStraight(6, 2, 3, 4, 5));
+            Assert.Equal(20, LargeStraight(2, 3, 4, 5, 6));
+            Assert.Equal(0, LargeStraight(1, 2, 2, 4, 5));
         }
 
         [Fact]
@@ -94,34 +95,34 @@ namespace Yatzy.Test
         [Fact]
         public void smallStraight()
         {
-            Assert.Equal(15, Yatzy1.Yatzy1.SmallStraight(1, 2, 3, 4, 5));
-            Assert.Equal(15, Yatzy1.Yatzy1.SmallStraight(2, 3, 4, 5, 1));
-            Assert.Equal(0, Yatzy1.Yatzy1.SmallStraight(1, 2, 2, 4, 5));
+            Assert.Equal(15, SmallStraight(1, 2, 3, 4, 5));
+            Assert.Equal(15, SmallStraight(2, 3, 4, 5, 1));
+            Assert.Equal(0, SmallStraight(1, 2, 2, 4, 5));
         }
 
         [Fact]
         public void three_of_a_kind()
         {
-            Assert.Equal(9, Yatzy1.Yatzy1.ThreeOfAKind(3, 3, 3, 4, 5));
-            Assert.Equal(15, Yatzy1.Yatzy1.ThreeOfAKind(5, 3, 5, 4, 5));
-            Assert.Equal(9, Yatzy1.Yatzy1.ThreeOfAKind(3, 3, 3, 3, 5));
+            Assert.Equal(9, ThreeOfAKind(3, 3, 3, 4, 5));
+            Assert.Equal(15, ThreeOfAKind(5, 3, 5, 4, 5));
+            Assert.Equal(9, ThreeOfAKind(3, 3, 3, 3, 5));
         }
 
         [Fact]
         public void two_Pair()
         {
-            Assert.Equal(16, Yatzy1.Yatzy1.TwoPair(3, 3, 5, 4, 5));
-            Assert.Equal(16, Yatzy1.Yatzy1.TwoPair(3, 3, 5, 5, 5));
+            Assert.Equal(16, TwoPair(3, 3, 5, 4, 5));
+            Assert.Equal(16, TwoPair(3, 3, 5, 5, 5));
         }
 
         [Fact]
         public void Yatzy_scores_50()
         {
             var expected = 50;
-            var actual = Yatzy1.Yatzy1.yatzy(4, 4, 4, 4, 4);
+            var actual = yatzy(4, 4, 4, 4, 4);
             Assert.Equal(expected, actual);
-            Assert.Equal(50, Yatzy1.Yatzy1.yatzy(6, 6, 6, 6, 6));
-            Assert.Equal(0, Yatzy1.Yatzy1.yatzy(6, 6, 6, 6, 3));
+            Assert.Equal(50, yatzy(6, 6, 6, 6, 6));
+            Assert.Equal(0, yatzy(6, 6, 6, 6, 3));
         }
     }
 }
